4990275277444eaca6e2a0bdff2c5cc9d283d506
naireeni <naireen.imtiaz@gmail.com>
2019-03-14 16:05:56 +0000

    edit

--- publishconf.py
+++ publishconf.py
@@ -20,5 +20,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = "naireeni-github-io"
+DISQUS_SITENAME = "naireeni-github-io"
 #GOOGLE_ANALYTICS = ""
